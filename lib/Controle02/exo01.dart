e93199502926f8e9043c995d43866d54396d18ac
Victoires <94651953+Victoire-s@users.noreply.github.com>
2025-12-03 16:33:27 +0100

    feat(add): exo 2

--- lib/Controle02/exo01.dart
+++ lib/Controle02/exo01.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:isolate';
 
 Future<void> task(int id) async {
   int delay = Random().nextInt(15) + 1;
@@ -10,10 +11,31 @@ Future<void> task(int id) async {
   print('task $id COMPLETED');
 }
 
+void taskIsolateEntryPoint(List<dynamic> args) {
+  final int id = args[0] as int;
+  final SendPort sendPort = args[1] as SendPort;
+
+  task(id).then((_) {
+    sendPort.send(id);
+  });
+}
+
 Future<void> main() async {
   print('début du main');
 
-  await task(89);
+  final receivePort = ReceivePort();
+
+  await Isolate.spawn(
+    taskIsolateEntryPoint,
+    [89, receivePort.sendPort],
+  );
+
+  print('main continue pendant que l’isolate travaille...');
+
+  final completedId = await receivePort.first as int;
+  print('Isolate terminé pour la task id: $completedId');
+
+  receivePort.close();
 
   print('fin du main');
 }
